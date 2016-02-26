647049358604863567905d81d41258cd7f3b1e04
JQ <yjq14114@gmail.com>
2016-02-26 23:17:53 +0800

    upload image demo

--- src/uploadImage/DwonLoadDemo.java
+++ src/uploadImage/DwonLoadDemo.java
@@ -1,15 +1,10 @@
 package uploadImage;
 
-
 /**
  * Created by 佳琦 on 2016/2/25.
  */
 public class DwonLoadDemo {
     public void dwonLoad()throws Exception{
-    }
 
-    public static void main(String[] args) throws Exception {
-        DwonLoadDemo d = new DwonLoadDemo();
-        d.dwonLoad();
     }
 }
